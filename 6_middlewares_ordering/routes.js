8e69f6e91ae462a4d786bca0d52101c5987de6f5
Shrawan Lakhe <shrawanlakhey@gmail.com>
2018-01-10 23:55:52 +0530

    arrow function implemented

--- 6_middlewares_ordering/routes.js
+++ 6_middlewares_ordering/routes.js
@@ -8,6 +8,11 @@ router.use((req, res, next) => {
    console.log('3 - handling api routes')
     next();
 });
+router.use('/courses/:courseId', (req, res, next) => {
+    console.log('4 - executing some mechanism for course detail api route.')
+    next();
+});
+
 //implement index route and send homepage content as response
 router.get('/', (req, res, next) => {
     res.send("This is home page");
@@ -19,7 +24,7 @@ router.route('/courses')
         });
     });
 router.use((req, res, next) => {
-    console.log('4 - middleware implemented only for /courses/:courseId route')
+    console.log('5 - middleware implemented only for /courses/:courseId route')
     next();
 });
 router.route('/courses/:courseId')
@@ -28,12 +33,12 @@ router.route('/courses/:courseId')
             _id: req.params.courseId
         });
     });
-router.param('courseId', function(req, res, next, id) {
-    console.log('5 - this middleware will be implemented for only routes having courseId as parameter no matter where it is declared before handling the intended route')
+router.param('courseId', (req, res, next, id) => {
+    console.log('6 - this middleware will be implemented for only routes having courseId as parameter no matter where it is declared before handling the intended route')
     next();
 });
 router.use((req, res, next) => {
-    console.log('6 - will not be called');
+    console.log('7 - will not be called');
     next();
 });
 module.exports = router;
